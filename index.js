14fa3d8a5fa033dd7b356d5d21e99a2b37b95219
TravisWilley <12624447-TravisWilley@users.noreply.replit.com>
2023-08-22 00:42:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let myhttp = require('http');
+
+
+let myserver = myhttp.createServer(
+  function( myrequest, myresponse ) {
+    console.log( myrequest.url );
+
+    let mytext;
+    if ( myrequest.url === "/sup" ) {
+      mytext = "Whats up buddy..";
+    } else {
+      mytext = "I don't know you!";
+    }
+
+    myresponse.writeHead( 200, { "Content-Type": "text/plain" } );
+
+    myresponse.end( mytext );
+  }
+);
+
+
+myserver.listen(8080, "0.0.0.0");
+
+console.log("server started");
